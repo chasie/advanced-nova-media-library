760ef289080d509dc14a795a348d7d5b31a61802
chasie <danil4863@gmail.com>
2022-01-21 12:04:15 +0300

    fix(index-field): Правка высоты

--- resources/js/components/fields/IndexField.vue
+++ resources/js/components/fields/IndexField.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="field.type === 'media'">
-    <img v-if="value" :src="imageUrl" style="object-fit: cover;" class="rounded w-64 h-8" />
+    <img v-if="value" :src="imageUrl" style="object-fit: cover;" class="rounded w-64 h-64" />
     <span v-else>&mdash;</span>
   </div>
   <div v-else>
