8eca4fc38ed20155e42bf393835d1b74158ba6b8
chasie <danil4863@gmail.com>
2022-01-21 11:37:46 +0300

    fix(index-field): Исправление импорта[3]

--- resources/js/components/fields/IndexField.vue
+++ resources/js/components/fields/IndexField.vue
@@ -5,7 +5,7 @@
       :index="index"
       @close="index = null">
     </CoolLightBox>
-    <img v-if="value" :src="imageUrl" style="object-fit: cover;" class="rounded-full w-64 h-64" />
+    <img v-if="value" :src="imageUrl" style="object-fit: cover;" class="rounded w-64 h-64" />
     <span v-else>&mdash;</span>
   </div>
   <div v-else>
@@ -17,8 +17,14 @@
 </template>
 
 <script>
+  import CoolLightBox from 'vue-cool-lightbox'
+  import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
+
   export default {
     props: ['resourceName', 'field'],
+    components: {
+      CoolLightBox,
+    },
     computed: {
       value() {
         return this.field.value[0];
