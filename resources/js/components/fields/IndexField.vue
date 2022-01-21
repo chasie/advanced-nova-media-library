812607544fca3f2637ed1dc446491b3e52b68adf
chasie <danil4863@gmail.com>
2022-01-21 11:33:15 +0300

    fix(index-field): Исправление импорта[1]

--- resources/js/components/fields/IndexField.vue
+++ resources/js/components/fields/IndexField.vue
@@ -17,14 +17,8 @@
 </template>
 
 <script>
-  import CoolLightBox from 'vue-cool-lightbox'
-  import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
-
   export default {
     props: ['resourceName', 'field'],
-    components: {
-      CoolLightBox,
-    },
     computed: {
       value() {
         return this.field.value[0];
